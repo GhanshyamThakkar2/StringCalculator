ba3860aa7e16014ae6534b9a38f0740170057006
GhanshyamThakkar2 <tfgpghanshyam@gmail.com>
2025-07-05 11:01:46 +0530

    added first test case of empty string with add function

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,11 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator
 {
-
+    public int add(String numbers) {
+        return 0; // placeholder
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,14 @@
 package calculator;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest
 {
-
+    @Test
+    public void testEmptyStringReturnsZero() {
+        Calculator calc = new Calculator();
+        assertEquals(0, calc.add(""));
+    }
 }
